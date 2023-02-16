9315cb5161852b02794dcffb8d517237424f0f13
Franco Coronel <v-fcoronel@trueaccord.com@trueaccord.com>
2023-02-16 13:06:51 -0300

    feat: added MUI table

--- src/components/CharactersTable.tsx
+++ src/components/CharactersTable.tsx
@@ -1,51 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ICharacters } from "../hooks/useFetchData";
-
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { Typography } from "@mui/material";
 export interface IParamsCharactersTable {
   characters: ICharacters[];
   term: string;
 }
 const CharactersTable = ({ characters, term }: IParamsCharactersTable) => {
   return (
-    <table className="table-characters">
-      <thead>
-        <tr>
-          <th>Image</th>
-          <th>Name</th>
-          <th>Description</th>
-        </tr>
-      </thead>
-      <tbody>
-        {characters
-          .filter((item) => {
-            if (term !== "") {
-              return item.name.toLowerCase().includes(term);
-            } else {
-              return item;
-            }
-          })
-          .map((item) => {
-            return (
-              <tr key={item.id}>
-                <td>
-                  <img
-                    src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
-                    alt="thumbnail"
-                    width={"200px"}
-                  />
-                </td>
-                <td>
-                  <Link to={`/character/${item.id}`}>{item.name}</Link>
-                </td>
-                <td>
-                  <p style={{ margin: 0 }}>{item.description}</p>
-                </td>
-              </tr>
-            );
-          })}
-      </tbody>
-    </table>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Image</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Description</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {characters
+            .filter((item) => {
+              if (term !== "") {
+                return item.name.toLowerCase().includes(term);
+              } else {
+                return item;
+              }
+            })
+            .map((item) => {
+              return (
+                <TableRow key={item.id}>
+                  <TableCell>
+                    <img
+                      src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                      alt="thumbnail"
+                      width={"200px"}
+                    />
+                  </TableCell>
+                  <TableCell sx={{ verticalAlign: "top" }}>
+                    <Stack>
+                      <Link to={`/character/${item.id}`}>{item.name}</Link>
+                    </Stack>
+                  </TableCell>
+                  <TableCell sx={{ verticalAlign: "top" }}>
+                    <Stack>
+                      <Typography style={{ margin: 0 }}>
+                        {item.description === "" ? (
+                          <Typography
+                            sx={{ fontStyle: "italic", color: "grey" }}
+                          >
+                            No description
+                          </Typography>
+                        ) : (
+                          item.description
+                        )}
+                      </Typography>
+                    </Stack>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
